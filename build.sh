d17854b5faad290f2e83f50f76018a90b9312b5d
system1 <braungoodson@gmail.com>
2014-01-06 16:47:25 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,30 @@
-echo "\033[0;34m"'           _____       _             _     '"\033[0m"
-echo "\033[0;34m"'          / __  \     | |           | |    '"\033[0m"
-echo "\033[0;34m"'  ___  ___`' / /'  ___| |_ __ _  ___| | __ '"\033[0m"
-echo "\033[0;34m"' / _ \/ __| / /   / __| __/ _` |/ __| |/ / '"\033[0m"
-echo "\033[0;34m"'|  __/ (__./ /___ \__ \ || (_| | (__|   <  '"\033[0m"
-echo "\033[0;34m"' \___|\___\_____/ |___/\__\__,_|\___|_|\_\ '"\033[0m"
-echo "\033[0;34m"'                                           '"\033[0m"
+echo "\033[0;34m"'    .___                              __                 __    '"\033[0m"
+echo "\033[0;34m"'  __| _/_______  __           _______/  |______    ____ |  | __'"\033[0m"
+echo "\033[0;34m"' / __ |/ __ \  \/ /  ______  /  ___/\   __\__  \ _/ ___\|  |/ /'"\033[0m"
+echo "\033[0;34m"'/ /_/ \  ___/\   /  /_____/  \___ \  |  |  / __ \\  \___|    < '"\033[0m"
+echo "\033[0;34m"'\____ |\___  >\_/           /____  > |__| (____  /\___  >__|_ \'"\033[0m"
+echo "\033[0;34m"'     \/    \/                    \/            \/     \/     \/'"\033[0m"
 
+sudo mkdir -p /data/db
 
 sudo apt-get update
-sudo apt-get install -y make
-sudo apt-get install -y g++
-sudo apt-get install -y git
-sudo apt-get install -y zsh
-sudo apt-get install -y mongodb
+sudo apt-get install zsh
+sudo apt-get install make
+sudo apt-get install g++
+sudo apt-get install git
+sudo apt-get install mongodb
 curl -O http://nodejs.org/dist/v0.10.24/node-v0.10.24-linux-x64.tar.gz
 tar -zxvf node-v0.10.24-linux-x64.tar.gz
+
+echo "\n# chsh override\n" >> ~/.bashrc
+echo "zsh\n" >> ~/.bashrc
+
+cat ./zshrc.conf >> ~/.zshrc
+
+echo ""
+echo "\033[0;34m"
+echo " TODO:";
+echo "  * ssh-keygen"
+echo "  * /etc/hosts"
+echo "\033[0m"
 
