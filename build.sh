cd2d43f2a1b034855407867beb0662aa3ab72494
Ubuntu <ubuntu@ip-172-31-38-246.us-west-2.compute.internal>
2014-01-06 05:50:33 +0000

    ok

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ echo "\033[0;34m"'                                           '"\033[0m"
 
 sudo apt-get update
 sudo apt-get install -y make
+sudo apt-get install -y g++
 sudo apt-get install -y git
 sudo apt-get install -y zsh
 sudo apt-get install -y mongodb
